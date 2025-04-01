6565f8a88e69f01ea23822f0ff4ccfd23c089f74
OniiCode <oniicoder@gmail.com>
2025-04-01 12:36:38 +0000

    Use best practice and use strata version number

--- resources/views/script.blade.php
+++ resources/views/script.blade.php
@@ -1,28 +1,30 @@
+@php use Helix\Lego\Facades\Strata; @endphp
+
 @if(Astrogoat\Datadog\Settings\DatadogSettings::isEnabled())
     @php
         $setting = app(Astrogoat\Datadog\Settings\DatadogSettings::class);
     @endphp
-{{--    @push('body')--}} {{-- Uncomment this line if you want to push the script to the end of the body --}}
-        <script>
-            (function(h,o,u,n,d) {
-                h=h[d]=h[d]||{q:[],onReady:function(c){h.q.push(c)}}
-                d=o.createElement(u);d.async=1;d.src=n
-                n=o.getElementsByTagName(u)[0];n.parentNode.insertBefore(d,n)
-            })(window,document,'script','https://www.datadoghq-browser-agent.com/us1/v6/datadog-rum.js','DD_RUM')
-            window.DD_RUM.onReady(function() {
-                window.DD_RUM.init({
-                    clientToken: '{{ $setting->client_token }}',
-                    applicationId: '{{ $setting->application_id }}',
-                    site: '{{ config('datadog.site') }}',
-                    service: '{{ config('datadog.service') }}',
-                    env: '{{ env('APP_ENV') }}',
-                    version: '6.6.2',
-                    sessionSampleRate: {{ $setting->session_sample_rate }},
-                    sessionReplaySampleRate: {{ $setting->session_replay_sample_rate }},
-                    defaultPrivacyLevel: '{{ $setting->default_privacy_level }}',
-                    trackUserInteractions: true,
-                });
-            })
-        </script>
-{{--    @endpush--}}
+    <script>
+        (function(h,o,u,n,d) {
+            h=h[d]=h[d]||{q:[],onReady:function(c){h.q.push(c)}}
+            d=o.createElement(u);d.async=1;d.src=n
+            n=o.getElementsByTagName(u)[0];n.parentNode.insertBefore(d,n)
+        })(window,document,'script','https://www.datadoghq-browser-agent.com/us1/v6/datadog-rum.js','DD_RUM')
+        window.DD_RUM.onReady(function() {
+            window.DD_RUM.init({
+                clientToken: '{{ $setting->client_token }}',
+                applicationId: '{{ $setting->application_id }}',
+                site: '{{ config('datadog.site') }}',
+                service: '{{ config('datadog.service') }}',
+                env: '{{ config('app.env') }}',
+                @if(Strata::appVersion())
+                    version: '{{ Strata::appVersion() }}'
+                @endif
+                sessionSampleRate: {{ $setting->session_sample_rate }},
+                sessionReplaySampleRate: {{ $setting->session_replay_sample_rate }},
+                defaultPrivacyLevel: '{{ $setting->default_privacy_level }}',
+                trackUserInteractions: true,
+            });
+        })
+    </script>
 @endif
